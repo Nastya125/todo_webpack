5b7ce11048c93b93c3e96f269982a987ebcb7ebc
Nastya125 <nastyatrue@mail.ru>
2023-03-10 19:23:16 +0300

    fix

--- src/js/weekList.js
+++ src/js/weekList.js
@@ -1,11 +1,11 @@
 const dayList = [
     {id:"sun", title: "Вс"},
-    {id:"mon", title: "Вс"},
-    {id:"tue", title: "Вс"},
-    {id:"wed", title: "Вс"},
-    {id:"thu", title: "Вс"},
-    {id:"fri", title: "Вс"},
-    {id:"sat", title: "Вс"},
+    {id:"mon", title: "Пн"},
+    {id:"tue", title: "Вт"},
+    {id:"wed", title: "Ср"},
+    {id:"thu", title: "Чт"},
+    {id:"fri", title: "Пт"},
+    {id:"sat", title: "Сб"},
 ]
 
 const weekList = document.createElement("ul");
@@ -14,13 +14,13 @@ let date = new Date();
 weekList.classList.add('week_list');
 for (let i = 0; i < dayList.length; i++) {
     const { id, title } = dayList[i];
-    weekList.innerHTML += `<li>${title}<input id="${id}" type="radio></li>`
+    weekList.innerHTML += `<li>${title}<input id="${id}" type="radio" name="week"></li>`
 }
 
 const week = [];
 for (let i = 0; i < dayList.length; i++) {
     const { id } = dayList[i];
-    const day = weekList.querySelector(`#${id}`)
+    const day = weekList.querySelector(`#${id}`);
     week.push(day);
 }
 
